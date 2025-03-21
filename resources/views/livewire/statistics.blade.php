59ea2476f0cda2eab5d348a21faa7b26d06e6602
Murtada Luqman <96062205+MurtadaDev20@users.noreply.github.com>
2025-03-21 21:50:26 +0300

    update statiscts

--- resources/views/livewire/statistics.blade.php
+++ resources/views/livewire/statistics.blade.php
@@ -1,28 +1,28 @@
 <div>
       <!-- إحصائيات الفعاليات -->
-<section class="bg-gray-900 py-6">
+<section class="bg-gray-100 py-6">
     <div class="container mx-auto px-4">
         <div class="grid grid-cols-1 sm:grid-cols-3 gap-4 text-center text-white">
             
             <!-- عدد الزوار -->
-            <div class="flex flex-col items-center bg-gray-800 p-4 rounded-md shadow-md w-full">
+            <div class="flex flex-col items-center bg-white p-4 rounded-md shadow-md w-full">
                 <i class="fas fa-users text-3xl text-blue-400 mb-2"></i>
-                <div class="text-2xl font-bold text-white" id="visitors">0</div>
-                <p class="text-blue-400 text-sm">عدد الزوار</p>
+                <div class="text-2xl font-bold text-gray-800 " id="visitors">0</div>
+                <p class="text-orange-500 font-bold text-sm">عدد الزوار</p>
             </div>
   
             <!-- عدد التصنيفات -->
-            <div class="flex flex-col items-center bg-gray-800 p-4 rounded-md shadow-md w-full">
+            <div class="flex flex-col items-center bg-white p-4 rounded-md shadow-md w-full">
                 <i class="fas fa-layer-group text-3xl text-green-400 mb-2"></i>
-                <div class="text-2xl font-bold text-white" id="categories">0</div>
-                <p class="text-green-400 text-sm">عدد التصنيفات</p>
+                <div class="text-2xl font-bold text-gray-800" id="categories">0</div>
+                <p class="text-orange-500 font-bold text-sm">عدد التصنيفات</p>
             </div>
   
             <!-- عدد الفعاليات -->
-            <div class="flex flex-col items-center bg-gray-800 p-4 rounded-md shadow-md w-full">
+            <div class="flex flex-col items-center bg-white p-4 rounded-md shadow-md w-full">
                 <i class="fas fa-calendar-alt text-3xl text-yellow-400 mb-2"></i>
-                <div class="text-2xl font-bold text-white" id="events">0</div>
-                <p class="text-yellow-400 text-sm">عدد الفعاليات</p>
+                <div class="text-2xl font-bold text-gray-800" id="events">0</div>
+                <p class="text-orange-500 font-bold text-sm">عدد الفعاليات</p>
             </div>
   
         </div>
